4a7a81be46bf12de13bafc2eb445fa15dbcb9b0d
dominiccheruiyot20-hue <dominiccheruiyot20@gmail.com>
2026-09-17 23:53:33 +0300

    Update settings.py

--- loanshield_api/settings.py
+++ loanshield_api/settings.py
@@ -69,12 +69,21 @@ if os.environ.get('DATABASE_URL'):
         )
     }
 else:
+    
+     import dj_database_url
+
+# DATABASE - fix for Render build
+if os.environ.get('DATABASE_URL'):
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
+    }
+else:
     DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.sqlite3',
             'NAME': BASE_DIR / 'db.sqlite3',
         }
-    }
+    }      
     
 AUTH_PASSWORD_VALIDATORS = [
     {'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',},
