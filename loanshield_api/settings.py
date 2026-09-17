81024d94785b3f3b5d15ee25d81ea6f5f6497f9a
dominiccheruiyot20-hue <dominiccheruiyot20@gmail.com>
2026-09-17 23:30:33 +0300

    Update settings.py

--- loanshield_api/settings.py
+++ loanshield_api/settings.py
@@ -56,14 +56,26 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'loanshield_api.wsgi.application'
 
-DATABASES = {
-    'default': dj_database_url.config(
-        default=os.environ.get('DATABASE_URL'),
-        conn_max_age=600,
-        ssl_require=True,
-    )
-}
+import dj_database_url  # make sure this is at top
 
+# ... other settings ...
+
+if os.environ.get('DATABASE_URL'):
+    DATABASES = {
+        'default': dj_database_url.config(
+            default=os.environ.get('DATABASE_URL'),
+            conn_max_age=600,
+            ssl_require=True,
+        )
+    }
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
+    
 AUTH_PASSWORD_VALIDATORS = [
     {'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',},
     {'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',},
