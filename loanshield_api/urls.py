197a286a0a8663b137bfa446964b3693e5f7bc56
dominiccheruiyot20-hue <dominiccheruiyot20@gmail.com>
2026-09-18 16:42:33 +0300

    Update urls.py

--- loanshield_api/urls.py
+++ loanshield_api/urls.py
@@ -3,5 +3,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('loanshield_api.fraud_detector.urls')),
+    path('api/', include('fraud_detector.urls')),
 ]
